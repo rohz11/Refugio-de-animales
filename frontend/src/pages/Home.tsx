edcca1773b793be841533c9d4d4b5803b405d35f
Vk.Sm <vm.0610.sm@gmail.com>
2025-07-03 00:53:22 +0200

    Agregando detalles al home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="home-bg">
       <div className="home-content">
-        <h1 className="home-title">¿Te falta un compañero en casa?</h1>
+        <h1 className="home-title">¿Y si el amor de tu vida tiene cuatro patas?</h1>
         <p className="home-subtitle">
-          Adopte un leal amigo en nuestro refugio (West Animal Side Center)
+          Dales un hogar. Gánate un amigo para siempre. Cada mascota tiene una historia y un corazón lleno de amor esperando por ti. Adoptar no solo cambia su vida… también cambiará la tuya. Explora, enamórate y da ese primer paso hacia una conexión única. Porque cuando adoptas, rescatas más que una vida: encuentras una amistad incondicional.
         </p>
         <Link to="/adopcion" className="adopt-btn">
           Adopta aquí
